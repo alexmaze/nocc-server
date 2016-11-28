136fc5503384ac478a842e23c46cceb7c9b40b6a
Alex (Jinghao) Yan <yanhao1991@gmail.com>
2016-11-28 22:07:28 +0800

    fix cannot login issue

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,7 @@ export function auth(req, res, next) {
   if (req.session.user) {
     console.log('登录的用户', req.session.user.name)
   }
-  if (req.method !== 'GET') {
+  if (req.method !== 'GET' && req.originalUrl !== '/api/session') {
     if (req.session.user === undefined || req.session.user.role !== 'admin') {
       res.status(403).end()
     }
